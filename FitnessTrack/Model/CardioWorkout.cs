f7597ddad72757c6dd9b0a5b72b69ecb61643054
HP <farishtasulaiman@gmail.com>
2024-10-25 11:06:33 +0200

    classerna cardioworkout + strengthworkout klar

--- FitnessTrack/Model/CardioWorkout.cs
+++ FitnessTrack/Model/CardioWorkout.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace FitnessTrack.Model
 {
-    internal class CardioWorkout
+    public class CardioWorkout : WorkOut
     {
+        public int Distance { get; set; }
+
+        public CardioWorkout(DateTime date, string type, TimeSpan duration, int caloriesBurned, string notes, int distance)
+            : base(date, type, duration, caloriesBurned, notes)
+        {
+            Distance = distance;
+        }
+
+        public override int CalculateCaloriesBurned()
+        {
+            return CaloriesBurned + Distance * 5;
+        }
     }
 }
--- FitnessTrack/Model/StrengthWorkout.cs
+++ FitnessTrack/Model/StrengthWorkout.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace FitnessTrack.Model
 {
-    internal class StrengthWorkout
+    public class StrengthWorkout : WorkOut
     {
+        public int Repetitions { get; set; }
+
+        public StrengthWorkout(DateTime date, string type, TimeSpan duration, int caloriesBurned, string notes, int repetitions)
+            : base(date, type, duration, caloriesBurned, notes)
+        {
+            Repetitions = repetitions;
+        }
+
+        public override int CalculateCaloriesBurned()
+        {
+            return CaloriesBurned + Repetitions * 2;
+        }
     }
 }
