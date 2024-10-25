b9909d738b3ebfef17c4822d0578811a6a293d50
HP <farishtasulaiman@gmail.com>
2024-10-25 10:58:22 +0200

    skapat min person class med property, konstruktor och signin metod

--- FitnessTrack/Model/Person.cs
+++ FitnessTrack/Model/Person.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace FitnessTrack.Model
 {
-    internal class Person
+    public abstract class Person
     {
+        // Property
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        // Konstruktor
+        public Person(string userName, string passWord)
+        {
+            UserName = userName;
+            PassWord = passWord;
+        }
+
+        // Abstrakt metod utan implementering, det är underklasserna som måste implementera den
+        public abstract void SignIn();
     }
 }
