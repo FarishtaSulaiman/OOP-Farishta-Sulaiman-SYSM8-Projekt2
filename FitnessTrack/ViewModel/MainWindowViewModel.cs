6646cdc8b35c5ac799a68ffbda02e2bb1c0827c8
HP <farishtasulaiman@gmail.com>
2024-10-25 12:22:54 +0200

    uppdaterat min mainwindowviewmodel enligt: egenskaper som binder data från mainwindow vad gäller användarnman och lösen, usermanager en instans som tas emot av splashscreen commands, en signin metod, forgorpasswordmetod och registermetod med logik

--- FitnessTrack/ViewModel/MainWindowViewModel.cs
+++ FitnessTrack/ViewModel/MainWindowViewModel.cs
@@ -1,52 +1,89 @@
 ﻿using FitnessTrack.Model;
 using FitnessTrack.MVVM;
+using FitnessTrack.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 
 namespace FitnessTrack.ViewModel
 {
-   public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase
     {
-        private readonly UserManager _userManager;
+        private readonly UserManager _userManager;  // För att hantera användare
 
+        // Egenskaper för användarnamn och lösenord
         public string Username { get; set; }
         public string Password { get; set; }
 
+        // Kommandon för knapparna
         public ICommand SignInCommand { get; }
         public ICommand ForgotPasswordCommand { get; }
         public ICommand RegisterCommand { get; }
 
+        // Konstruktor som tar emot en UserManager-instans
         public MainWindowViewModel(UserManager userManager)
         {
             _userManager = userManager;
+
+            // Initialisera kommandon och koppla till funktioner
             SignInCommand = new RelayCommand(SignIn);
             ForgotPasswordCommand = new RelayCommand(ForgotPassword);
             RegisterCommand = new RelayCommand(Register);
         }
 
+        // Logik för Sign In-knappen
         private void SignIn(object parameter)
         {
+            // Kontrollera om användarnamn och lösenord stämmer
             var user = _userManager.GetUserByCredentials(Username, Password);
 
             if (user != null && user.PassWord == Password)
             {
+                // Om inloggningen lyckas, öppna WorkoutWindow
                 var workoutsWindow = new WorkoutWindow(user);
                 workoutsWindow.Show();
+
+                // Stäng MainWindow
                 Application.Current.Windows[0]?.Close();
             }
             else
             {
+                // Visa felmeddelande om inloggningen misslyckas
                 MessageBox.Show("Fel användarnamn eller lösenord.", "Fel", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        private void ForgotPassword(object parameter) { /* Din logik */ }
+        // Logik för Forgot Password-knappen
+        private void ForgotPassword(object parameter)
+        {
+            var user = _userManager.GetUserByUsername(Username);
 
-        private void Register(object parameter) { /* Din logik */ }
+            if (user != null)
+            {
+                // Säkerhetsfråga för att återställa lösenord
+                string answer = Microsoft.VisualBasic.Interaction.InputBox(user.SecurityQuestion, "Säkerhetsfråga");
+                user.ResetPassword(answer);
+            }
+            else
+            {
+                MessageBox.Show("Användarnamnet existerar inte.", "Fel", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // Logik för Register-knappen
+        private void Register(object parameter)
+        {
+            // Öppna RegisterWindow för att skapa ny användare
+            var registerWindow = new RegisterWindow();
+            registerWindow.Show();
+
+            // Stäng MainWindow
+            Application.Current.Windows[0]?.Close();
+        }
     }
 }
